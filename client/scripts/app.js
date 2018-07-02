750a6a9335969e753688582e29d623c3fe122a2f
courtchao <couchao@ucdavis.edu>
2018-07-02 12:31:47 -0700

    Get 'GET' requests working; start on 'POST' requests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,7 +3,7 @@ var app = {
 
   //TODO: The current 'handleUsernameClick' function just toggles the class 'friend'
   //to all messages sent by the user
-  server: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+  server: 'http://127.0.0.1:3000/',
   username: 'anonymous',
   roomname: 'lobby',
   lastMessageId: 0,
@@ -37,7 +37,7 @@ var app = {
 
   send: function(message) {
     app.startSpinner();
-
+    // debugger;
     // POST the message to the server
     $.ajax({
       url: app.server,
@@ -61,11 +61,11 @@ var app = {
     $.ajax({
       url: app.server,
       type: 'GET',
-      data: { order: '-createdAt' },
+      // data: { order: '-createdAt' },
       success: function(data) {
         // Don't bother if we have nothing to work with
         if (!data.results || !data.results.length) { return; }
-
+        
         // Store messages for caching later
         app.messages = data.results;
 
